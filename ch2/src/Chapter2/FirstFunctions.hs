4d05b65ee5357cbea754ebdaeee1dbe4f4b7c11d
Jan Skowronski <crianonim@gmail.com>
2019-10-08 08:29:16 +0100

    Maxmin and maxmin2

--- ch2/src/Chapter2/FirstFunctions.hs
+++ ch2/src/Chapter2/FirstFunctions.hs
@@ -16,3 +16,22 @@ reverse2 list = if null list
     then []
     else reverse2 (tail list) +++ [head list]
 
+maxmin list = if null (tail list)
+    then (head list,head list)
+    else ( if (head list) > fst (maxmin (tail list)) 
+        then head list
+        else fst (maxmin(tail list))
+        
+        , if (head list ) < snd (maxmin (tail list))
+            then head list
+            else snd (maxmin (tail list))
+    )
+
+maxmin2 list = let h = head list
+        in if null (tail list) 
+            then (h,h)
+            else (if h > t_max then h else t_max
+            ,if h<t_min then h else t_min)
+            where t = maxmin2 (tail list) {- has to start in same column -}
+                  t_max=fst t
+                  t_min=snd t
